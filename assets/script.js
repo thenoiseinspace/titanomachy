6ff30a978059e2f537ce667eb8d2a185a0442048
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-04-17 19:13:55 -0400

    adding titan constructor classes

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+// Use activity 21 and 22
+
+//Titan characters
+
+class TitanCharacter {
+    // Just like constructor functions, classes can accept arguments
+    constructor(titanName, titanDescription, titanStrikePoints, gaiaChild, fights, turncoat) {
+      this.titanName = titanName;
+      this.titanDescription = titanDescription;
+      this.titanStrikePoints = titanStrikePoints; 
+      this.gaiaChild = gaiaChild; 
+      this.fights = fights; 
+      this.turncoat = turncoat; 
+    }
+  
+    // printInfo() {
+    //   console.log(`Area: ${this.area}`);
+    //   console.log(`Perimeter: ${this.perimeter}`);
+    // }
+  }
+  
+//   const shape = new Shape(25, 25);
+  
+//   shape.printInfo();
+
+//Titan character cards
+const atlas = new TitanCharacter (atlas, "general skilled in philosophy math and astronomy", 10, false, true, false); 
+
+const iapetus = new TitanCharacter (iapetus, "good with spears", 6, true, true, false); 
+
+const menoetius = new TitanCharacter (menoetius, "violent anger and rash action", 8, false, true, false); 
+
+const crius = new TitanCharacter (crius, "pillar of the sky", 7, true, true, false); 
+
+const coeus = new TitanCharacter (coeus, "pillar intellect stopped by dogs", 9, true, true, false); 
+
+const hyperion = new TitanCharacter (hyperion, "sun god", 8, true, true, false); 
+
+const cronus = new TitanCharacter (cronus, "king of titans", 0, true, true, false); 
+
+const astraeus = new TitanCharacter (astraeus, "god of dusk", 6, false, true, false); 
+
+const perses = new TitanCharacter (perses, "god of destruction", 9, false, true, false); 
+
+const gaia = new TitanCharacter (gaia, "mother goddess", 0, false, false, false); 
+
+const prometheus = new TitanCharacter (prometheus, "trickster and intellectual", 8, false, true, true); 
+
+const themis = new TitanCharacter (themis, "goddess of justice", 6, true, true, true); 
+
+const cyclops = new TitanCharacter (cyclops, "cyclops", 7, true, true, true); 
+
+const hecatoncheire = new TitanCharacter (hecatoncheire, "hundred hands", 7, true, true, true); 
+
+
+
+  
